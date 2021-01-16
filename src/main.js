d13a3bed5b9c3f73033810f3cb21440c5a2b6409
mrmlik12 <mrmlik12sup@gmail.com>
2021-01-16 17:39:45 +0100

    add vue.config.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
+import Chartkick from 'vue-chartkick';
+import Chart from 'chart.js';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import Chartkick from 'vue-chartkick'
-import Chart from 'chart.js'
 
 Vue.config.productionTip = false;
-Vue.use(Chartkick.use(Chart))
+Vue.use(Chartkick.use(Chart));
 
 new Vue({
   router,
